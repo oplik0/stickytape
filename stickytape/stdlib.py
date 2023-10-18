a854265223832c22eb2b2469a5eb001eab1aeb5d
Opliko <opliko.reg@protonmail.com>
2023-10-18 17:00:32 +0200

    fix: use dots

--- stickytape/stdlib.py
+++ stickytape/stdlib.py
@@ -2,7 +2,7 @@ import sys
 
 
 def is_stdlib_module(module_name):
-    return any([module in _stdlib_modules for module in module_name.split('/')])
+    return any([module in _stdlib_modules for module in module_name.split('.')])
 
 
 _stdlib_modules = set([
