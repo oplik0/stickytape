14fbeab896d4b8d44dc7f33288c179f17199a736
Opliko <opliko.reg@protonmail.com>
2023-10-19 10:30:59 +0200

    fix: frozenset is not a function

--- stickytape/stdlib.py
+++ stickytape/stdlib.py
@@ -5,7 +5,7 @@ def is_stdlib_module(module_name):
     return any([module in _stdlib_modules for module in module_name.split('.')])
 
 
-_stdlib_modules =  sys.stdlib_module_names()
+_stdlib_modules =  sys.stdlib_module_names
 
 
 if sys.version_info[0] >= 3:
