170716d718a7df5639f3d72deedad788766ddb01
Opliko <opliko.reg@protonmail.com>
2023-10-19 10:34:35 +0200

    fix: only look at main package

--- stickytape/stdlib.py
+++ stickytape/stdlib.py
@@ -2,7 +2,7 @@ import sys
 
 
 def is_stdlib_module(module_name):
-    return any([module in _stdlib_modules for module in module_name.split('.')])
+    return  module_name.split('.')[0] in _stdlib_modules
 
 
 _stdlib_modules =  sys.stdlib_module_names
