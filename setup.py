c50b7fdc4486a5205bec62065c1ccf2d7256190e
Opliko <opliko.reg@protonmail.com>
2023-10-23 15:02:37 +0200

    chore: bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='stickytape',
-    version='0.3.0',
+    version='0.3.1',
     description='Convert Python packages into a single script',
     long_description=read("README.rst"),
     author='Michael Williamson',
