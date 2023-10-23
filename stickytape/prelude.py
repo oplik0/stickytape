100a67f5ebf47cb7f1b907c9e186584602a1d153
Opliko <opliko.reg@protonmail.com>
2023-10-23 15:04:50 +0200

    fix: don't use old aliases

--- stickytape/prelude.py
+++ stickytape/prelude.py
@@ -1,16 +1,16 @@
 global __stickytape_extract_archive
 def __stickytape_extract_archive(archive_data, hash):
-    from zipfile import ZipFile
+    from zipfile import ZipFile, ZIP_DEFLATED
     from io import BytesIO 
     from pathlib import Path
     import os
 
-    dir_path = Path(__stickytape_os.environ.get("XDG_CACHE_HOME", Path.home() / ".cache")) / "mn2" / "dependencies"
+    dir_path = Path(os.environ.get("XDG_CACHE_HOME", Path.home() / ".cache")) / "mn2" / "dependencies"
     dir_path.mkdir(parents=True, exists_ok=True)
     hash_file = dir_path / "hash.txt"
     if not hash_file.exists() or hash_file.read_text(encoding="utf-8").strip() != hash:
-        buffer = __stickytape_bytesio(archive_data)
-        archive = __stickytape_zipfile(buffer)
+        buffer = BytesIO(archive_data, mode="r", compression=ZIP_DEFLATED)
+        archive = ZipFile(buffer)
         archive.extractall(dir_path)
         hash_file.write_text(hash)
 
