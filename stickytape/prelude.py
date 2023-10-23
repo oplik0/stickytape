03478727402624fbac9a56ed7695f0a6b75777c4
Opliko <opliko.reg@protonmail.com>
2023-10-23 15:05:54 +0200

    fix: typo

--- stickytape/prelude.py
+++ stickytape/prelude.py
@@ -6,7 +6,7 @@ def __stickytape_extract_archive(archive_data, hash):
     import os
 
     dir_path = Path(os.environ.get("XDG_CACHE_HOME", Path.home() / ".cache")) / "mn2" / "dependencies"
-    dir_path.mkdir(parents=True, exists_ok=True)
+    dir_path.mkdir(parents=True, exist_ok=True)
     hash_file = dir_path / "hash.txt"
     if not hash_file.exists() or hash_file.read_text(encoding="utf-8").strip() != hash:
         buffer = BytesIO(archive_data, mode="r", compression=ZIP_DEFLATED)
